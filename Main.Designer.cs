b8627b40bebd18c729126e6993618f347eb9e7f2
BevizLaszlo <bevizlaci@gmail.com>
2023-11-17 14:06:17 +0100

    Add project files.

---
+++ Main.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace NumberMatrixGame
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GamePanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // GamePanel
+            // 
+            this.GamePanel.Location = new System.Drawing.Point(237, 68);
+            this.GamePanel.Name = "GamePanel";
+            this.GamePanel.Size = new System.Drawing.Size(300, 300);
+            this.GamePanel.TabIndex = 0;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GamePanel);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel GamePanel;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberMatrixGame
+{
+    public partial class Main : Form
+    {
+        enum Direction { Top, Right, Bottom, Left }
+        Random rnd = new Random();
+        int[,] numMatrix = new int[5, 5];
+
+        private void InitializeNumMatrix()
+        {
+            for (int i = 1; i <= 3; i++)
+                for (int j = 1; j <= 3; j++)
+                {
+                    numMatrix[i, j] = 5;
+                }
+            numMatrix[3, 3] = 0;
+        }
+
+        private void ButtonsRender()
+        {
+            for (int i = 0; i < numMatrix.GetLength(0); i++)
+                for (int j = 0; j < numMatrix.GetLength(1); j++)
+                    if (numMatrix[i, j] != 0)
+                    {
+
+                        Button btn = new Button()
+                        {
+                            Text = numMatrix[i, j].ToString(),
+                            Width = 100,
+                            Height = 100,
+                            Top = (i - 1) * 100,
+                            Left = (j - 1) * 100,
+                            Tag = new int[i, j]
+                        };
+                        btn.Click += Btn_Click;
+                        GamePanel.Controls.Add(btn);
+                    }
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (true)
+            {
+                AnimateButton(thisBtn, Direction.Bottom);
+            }
+        }
+
+        public Main()
+        {
+            InitializeComponent();
+            InitializeNumMatrix();
+            ButtonsRender();
+        }
+
+        private void AnimateButton(Button btn, Direction d)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                switch (d)
+                {
+                    case Direction.Top:
+                        btn.Top -= 10;
+                        break;
+                    case Direction.Right:
+                        btn.Left += 10;
+                        break;
+                    case Direction.Bottom:
+                        btn.Top += 10;
+                        break;
+                    case Direction.Left:
+                        btn.Left -= 10;
+                        break;
+                    default:
+                        break;
+                }
+                Thread.Sleep(3);
+            }
+        }
+
+        private void IsThereEmptySquare(int x, int y)
+        {
+
+            if (numMatrix[x - 1, y] == 0 && x - 1 > 0) return true;
+        }
+    }
+}

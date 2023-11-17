042e675d1b697f0d1b0399eced1864214e39a655
László Bevíz <bevizlaci@gmail.com>
2023-11-17 17:02:24 +0100

    upd

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -29,30 +29,49 @@
         private void InitializeComponent()
         {
             this.GamePanel = new System.Windows.Forms.Panel();
+            this.winLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // GamePanel
             // 
+            this.GamePanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(72)))), ((int)(((byte)(72)))));
+            this.GamePanel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.GamePanel.Location = new System.Drawing.Point(237, 68);
             this.GamePanel.Name = "GamePanel";
             this.GamePanel.Size = new System.Drawing.Size(300, 300);
             this.GamePanel.TabIndex = 0;
             // 
+            // winLabel
+            // 
+            this.winLabel.AutoSize = true;
+            this.winLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.winLabel.ForeColor = System.Drawing.Color.DarkGreen;
+            this.winLabel.Location = new System.Drawing.Point(324, 26);
+            this.winLabel.Name = "winLabel";
+            this.winLabel.Size = new System.Drawing.Size(129, 25);
+            this.winLabel.TabIndex = 1;
+            this.winLabel.Text = "YOU WON!";
+            this.winLabel.Visible = false;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.winLabel);
             this.Controls.Add(this.GamePanel);
             this.Name = "Main";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel GamePanel;
+        private System.Windows.Forms.Label winLabel;
     }
 }
 
--- Main.cs
+++ Main.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,15 +15,22 @@ namespace NumberMatrixGame
     public partial class Main : Form
     {
         enum Direction { Top, Right, Bottom, Left }
+        Direction currentTurn;
         Random rnd = new Random();
         int[,] numMatrix = new int[5, 5];
 
         private void InitializeNumMatrix()
         {
+            List<int> nums = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 };
             for (int i = 1; i <= 3; i++)
                 for (int j = 1; j <= 3; j++)
                 {
-                    numMatrix[i, j] = 5;
+                    if (i != 3 || j != 3)
+                    {
+                        int index = rnd.Next(nums.Count);
+                        numMatrix[i, j] = nums[index];
+                        nums.RemoveAt(index);
+                    }
                 }
             numMatrix[3, 3] = 0;
         }
@@ -41,20 +49,12 @@ namespace NumberMatrixGame
                             Height = 100,
                             Top = (i - 1) * 100,
                             Left = (j - 1) * 100,
-                            Tag = new int[i, j]
+                            Tag = $"{i}{j}",
+                            BackColor = Color.White,
                         };
                         btn.Click += Btn_Click;
                         GamePanel.Controls.Add(btn);
                     }
-        }
-
-        private void Btn_Click(object sender, EventArgs e)
-        {
-            Button thisBtn = (Button)sender;
-            if (true)
-            {
-                AnimateButton(thisBtn, Direction.Bottom);
-            }
         }
 
         public Main()
@@ -62,6 +62,49 @@ namespace NumberMatrixGame
             InitializeComponent();
             InitializeNumMatrix();
             ButtonsRender();
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            string coordinates = (string)thisBtn.Tag;
+            int x = int.Parse(coordinates[0].ToString());
+            int y = int.Parse(coordinates[1].ToString());
+
+            if (IsThereEmptySquare(x, y))
+            {
+                AnimateButton(thisBtn, currentTurn);
+                int[] newPlace = new int[2];
+
+                switch (currentTurn)
+                {
+                    case Direction.Top:
+                        thisBtn.Tag = $"{x - 1}{y}";
+                        newPlace = new int[] { x - 1, y };
+                        break;
+                    case Direction.Right:
+                        thisBtn.Tag = $"{x}{y + 1}";
+                        newPlace = new int[] { x, y + 1 };
+                        break;
+                    case Direction.Bottom:
+                        thisBtn.Tag = $"{x + 1}{y}";
+                        newPlace = new int[] { x + 1, y };
+                        break;
+                    case Direction.Left:
+                        thisBtn.Tag = $"{x}{y - 1}";
+                        newPlace = new int[] { x, y - 1 };
+                        break;
+                    default:
+                        break;
+                }
+
+                int temp = numMatrix[x, y];
+                numMatrix[x, y] = 0;
+                numMatrix[newPlace[0], newPlace[1]] = temp;
+
+                if (CheckForWin())
+                    winLabel.Visible = true;
+            }
         }
 
         private void AnimateButton(Button btn, Direction d)
@@ -89,10 +132,43 @@ namespace NumberMatrixGame
             }
         }
 
-        private void IsThereEmptySquare(int x, int y)
+        private bool IsThereEmptySquare(int x, int y)
         {
+            if (numMatrix[x - 1, y] == 0 && x - 1 > 0)
+            {
+                currentTurn = Direction.Top;
+                return true;
+            }
+            if (numMatrix[x + 1, y] == 0 && x + 1 < 4)
+            {
+                currentTurn = Direction.Bottom;
+                return true;
+            }
+            if (numMatrix[x, y - 1] == 0 && y - 1 > 0)
+            {
+                currentTurn = Direction.Left;
+                return true;
+            }
+            if (numMatrix[x, y + 1] == 0 && y + 1 < 4)
+            {
+                currentTurn = Direction.Right;
+                return true;
+            }
+            return false;
+        }
 
-            if (numMatrix[x - 1, y] == 0 && x - 1 > 0) return true;
+        private bool CheckForWin()
+        {
+            int checker = 1;
+
+            for (int i = 1; i < numMatrix.GetLength(0) - 1; i++)
+                for (int j = 1; j < numMatrix.GetLength(1) - 1; j++)
+                {
+                    if ((i != 3 || j != 3) && numMatrix[i, j] != checker) return false;
+                    checker++;
+                }
+
+            return true;
         }
     }
 }
